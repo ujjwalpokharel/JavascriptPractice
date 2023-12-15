9b71e1f65a2093c50f283752a327d022781f4e15
Ujjwal Pokharel <ujjwalppokharel@gmail.com>
2023-12-15 10:50:32 +0545

    feat(jvpractice): practice object pass by refrence

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { ArrayMapPractice } from "./Component/array-map-practice";
+import { ObjectPractice } from "./Component/object-practice";
 export const App =()=>{
   return(
+    <div>
+
     <ArrayMapPractice/>
+    <ObjectPractice/>
+    </div>
   )
 }
